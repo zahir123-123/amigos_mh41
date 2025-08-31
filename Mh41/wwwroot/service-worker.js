d051a47ee4822f08c4a722c31686cacdabffd093
zubair <raka99449@gmail.com>
2025-08-31 22:18:14 +0530

    Added all project files

---
+++ Mh41/wwwroot/service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'mh41-cache-v2';
+const urlsToCache = [
+  '/',
+  '/Index',
+  '/css/site.css',
+  '/js/site.js',
+  '/assets/car-banner.jpg',
+  '/assets/garagebanner.jpg',
+  '/assets/car-wash.png',
+  // Add more assets as needed
+];
+const OFFLINE_URL = '/offline.html'; // Assume we have an offline page
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          })
+          .catch(() => {
+            return caches.match(OFFLINE_URL);
+          });
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
